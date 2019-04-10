f78c091b532a3cbaa6154f75c0104d1505e01566
Darien Shannon <darien.shannon@gmail.com>
2019-04-09 23:23:42 -0700

    Bom Notes

--- src/bom-tools-library/NotesToExcel.fs
+++ src/bom-tools-library/NotesToExcel.fs
@@ -1,122 +1,59 @@
 ﻿module DESign.BomTools.NotesToExcel
  
-open DocumentFormat.OpenXml.Packaging
-open DocumentFormat.OpenXml.Spreadsheet
-open DocumentFormat.OpenXml
 open DESign.BomTools.Domain
+open OfficeOpenXml
+open System.IO
+open DocumentFormat.OpenXml.Spreadsheet
+
 
 
 let getParticularNotesByMark (job : Job) =
     let joistNotes =
         job.Joists
-        |> Seq.map
+        |> Seq.collect
             (fun joist ->
-                let notes = job |> Job.getJoistParticularNotes joist
-                notes
+                job
+                |> Job.getJoistParticularNotes joist
                 |> Seq.map
-                    (fun note -> joist.Mark, note.Note))
-        |> Seq.concat
+                    (fun note -> joist.Mark, note.Note, note.ID))
 
     let girderNotes =
         job.Girders
-        |> Seq.map
+        |> Seq.collect
             (fun girder ->
-                let notes = job |> Job.getGirderParticularNotes girder
-                notes
+                job |> Job.getGirderParticularNotes girder
                 |> Seq.map
-                    (fun note -> girder.Mark, note.Note))
-       |> Seq.concat
+                    (fun note -> girder.Mark, note.Note, note.ID))
 
     let allNotes = [girderNotes;joistNotes] |> Seq.concat
     allNotes
 
-let getParticularNotesByNote (job : Job) =
-    let joistNotes =
-        job.Joists
-        |> Seq.map
-            (fun joist ->
-                let notes = job |> Job.getJoistParticularNotes joist
-                notes
-                |> Seq.map
-                    (fun note -> (note, joist.Mark)))
-        |> Seq.concat
-    let girderNotes =
-        job.Girders
-        |> Seq.map
-            (fun girder ->
-                let notes = job |> Job.getGirderParticularNotes girder
-                notes
-                |> Seq.map
-                    (fun note -> (note, girder.Mark)))
-        |> Seq.concat
-    let allNotes =
-        [girderNotes;joistNotes]
-        |> Seq.concat
-        |> Seq.groupBy (fun (note, mark) -> note)
-        |> Seq.map
-            (fun (note, notesAndMarks) ->
-                let marks =
-                    notesAndMarks
-                    |> Seq.map (fun(_, mark) -> mark)
-                (note, marks))
-        |> Seq.sortBy (fun ((note, _) : (Note*_)) -> note.ID.ToCharArray())
-    allNotes
 
-let createCellReference (header:string) (index:int) =
-    StringValue(header + string(index))
 
-let createTextCell text (header:string) (index:int) =
-    let cell = new Cell()
-    cell.CellReference <- createCellReference header index
-    cell.DataType <- EnumValue(CellValues.InlineString)
-    let inlineString = new InlineString()
-    let t = new Text(Text = text)
-    t |> inlineString.AppendChild |> ignore
-    inlineString |> cell.AppendChild |> ignore
-    cell :> OpenXmlElement
-
-let particularNotesByMarkAsSheetData (job : Job) =
-    let particularNotesByMark = job |> getParticularNotesByMark
-    let sheetData = new SheetData()
-    let headerRow = new Row(RowIndex = UInt32Value(uint32(1)))
-    createTextCell "Mark" "A" 1 |> headerRow.Append
-    createTextCell "Note" "B" 1 |> headerRow.Append
-    (headerRow :> OpenXmlElement) |> sheetData.AppendChild |> ignore
-    particularNotesByMark
+let CreateBomInfoSheetFromJob (job : Job) =
+    let package = new ExcelPackage()
+    let notesByMarkSheet = package.Workbook.Worksheets.Add("Notes By Mark")
+    notesByMarkSheet.Cells.[1,1].Value <- "Mark"
+    notesByMarkSheet.Cells.[1,2].Value <- "Note"
+    notesByMarkSheet.Cells.[1,3].Value <- "Note Id"
+    let notesByMark = job |> getParticularNotesByMark
+    notesByMark
     |> Seq.iteri
-        (fun i (mark, note) ->
-            let row = new Row(RowIndex = UInt32Value(uint32(i + 2)))
-            let markCell = createTextCell mark "A" (i + 2)
-            let noteCell = createTextCell note "B" (i + 2)
-            markCell |> row.Append
-            noteCell |> row.Append
-            (row :> OpenXmlElement) |> sheetData.AppendChild |> ignore )
-    sheetData
+        (fun i (mark, note, noteId) ->
+            notesByMarkSheet.Cells.[i + 2, 1].Value <- mark
+            notesByMarkSheet.Cells.[i + 2, 2].Value <- note
+            notesByMarkSheet.Cells.[i + 2, 3].Value <- noteId)
 
- 
-    
-    
-let createSpreadsheet (filepath:string) ( listOfSheetNamesAndSheetData: (string*SheetData) list) =
-    using (SpreadsheetDocument.Create(filepath, SpreadsheetDocumentType.Workbook))
-        (fun spreadsheetDocument ->
-            let workbookPart = spreadsheetDocument.AddWorkbookPart(Workbook = new Workbook())
-            let sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets())
-            listOfSheetNamesAndSheetData
-            |> List.iteri
-                (fun i (sheetName, sheetData) ->                  
-                    let worksheetPart = workbookPart.AddNewPart<WorksheetPart>()
-                    worksheetPart.Worksheet <- new Worksheet(sheetData:> OpenXmlElement)
-                    let sheet = new Sheet( Id = StringValue(spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart)),
-                                           SheetId = UInt32Value(uint32 i),
-                                           Name = StringValue(sheetName))
-                    [sheet :> OpenXmlElement] |> sheets.Append ))
+    let lastRow = notesByMarkSheet.Dimension.End.Row
+    let tableRange = notesByMarkSheet.Cells.[1, 1, lastRow, 3]
+    let tableName = "tblBomInfo"
+    let table =notesByMarkSheet.Tables.Add(tableRange, tableName)
+    table.TableStyle <- Table.TableStyles.Light8
+    tableRange.AutoFitColumns()
 
-let createParticularNotesSpreadSheet (outputPath : string) (job : Job) =
-    let particularNotesAsSheetData = job |> particularNotesByMarkAsSheetData
-    createSpreadsheet
-        outputPath
-        ["Particular Notes By Mark", particularNotesAsSheetData
-         "Particular Notes By Mark 2", particularNotesAsSheetData]
-      
+    package
     
+
+
+
 
--- test/Test.fsx
+++ test/Test.fsx
@@ -3,6 +3,7 @@
 #r @"NETStandard.Library.NETFRamework/build/net461/lib/netstandard.dll"
 #r @"DocumentFormat.OpenXml/lib/net46/DocumentFormat.OpenXml.dll"
 #r @"System.IO.Packaging/lib/net46/System.IO.Packaging.dll"
+#r @"EPPlus/lib/net40/EPPlus.dll"
 #r @"WindowsBase"
 #r @"../src/bom-tools-library/bin/debug/netstandard2.0/bom-tools.dll"
 
@@ -17,108 +18,9 @@ open DESign.BomTools.NotesToExcel
 
 let bomFileName = @"C:\Users\darien.shannon\code\bom-tools\testBOMs\testbom1.xlsm"
 
-
-let printGeneralNotes () =
-    use bom = GetBom bomFileName
-    let particularNotes = GetGeneralNotes bom
-    particularNotes
-    |> Seq.iter
-        (fun note -> printfn "%s" note.Note )
-
-
-
-let printParticularNotes () =
-    use bom = GetBom bomFileName
-    let particularNotes = GetParticularNotes bom
-    particularNotes
-    |> Seq.iter
-        (fun note -> printfn "%s" note.Note )
-
-let printLoads () =
-    use bom = GetBom bomFileName
-    let loads = GetLoads bom
-    loads |> Seq.iter (fun load -> printfn "%A" load)
-
-let printJoists () =
-    use bom = GetBom bomFileName
-    let joists = GetJoists bom
-    joists |> Seq.iter (fun joist -> printfn "%A" joist)
-
-let printGirders () =
-    use bom = GetBom bomFileName
-    let girders = GetGirders bom
-    girders
-    |> Seq.filter (fun girder -> girder.Mark = "G17")
-    |> Seq.iter (fun girder -> printfn "%A" girder)
-
-let getParticularNotesByMark (job : Job) =
-    let joistNotes =
-        job.Joists
-        |> Seq.map
-            (fun joist ->
-                let notes = job |> Job.getJoistParticularNotes joist
-                notes
-                |> Seq.map
-                    (fun note -> joist.Mark, note.Note))
-        |> Seq.concat
-
-    let girderNotes =
-        job.Girders
-        |> Seq.map
-            (fun girder ->
-                let notes = job |> Job.getGirderParticularNotes girder
-                notes
-                |> Seq.map
-                    (fun note -> girder.Mark, note.Note))
-       |> Seq.concat
-
-    let allNotes = [girderNotes;joistNotes] |> Seq.concat
-    allNotes
-
-let getParticularNotesByNote (job : Job) =
-    let joistNotes =
-        job.Joists
-        |> Seq.map
-            (fun joist ->
-                let notes = job |> Job.getJoistParticularNotes joist
-                notes
-                |> Seq.map
-                    (fun note -> (note, joist.Mark)))
-        |> Seq.concat
-    let girderNotes =
-        job.Girders
-        |> Seq.map
-            (fun girder ->
-                let notes = job |> Job.getGirderParticularNotes girder
-                notes
-                |> Seq.map
-                    (fun note -> (note, girder.Mark)))
-        |> Seq.concat
-    let allNotes =
-        [girderNotes;joistNotes]
-        |> Seq.concat
-        |> Seq.groupBy (fun (note, mark) -> note)
-        |> Seq.map
-            (fun (note, notesAndMarks) ->
-                let marks =
-                    notesAndMarks
-                    |> Seq.map (fun(_, mark) -> mark)
-                (note, marks))
-        |> Seq.sortBy (fun ((note, _) : (Note*_)) -> note.ID.ToCharArray())
-
-    allNotes
-
-let particularNotesByMarkToCsv () =
+let getBomInfo () =
     use bom = GetBom bomFileName
     let job = GetJob bom
-    job |> createParticularNotesSpreadSheet @"C:\Users\darien.shannon\code\bom-tools\test\particularNotesByMark.xlsx"
+    createBomInfoSheet job @"C:\Users\darien.shannon\code\bom-tools\test\bomInfo.xlsx"
                 
-
-printGeneralNotes ()
-printParticularNotes ()
-printLoads ()
-printJoists ()
-printGirders ()
-printParticularNotes ()
-particularNotesByMarkToCsv()
-
+getBomInfo()
